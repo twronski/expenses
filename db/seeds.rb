6a9934470505b87e293f7579b362137e1aba6de7
Thiago Wronski <wronski79@gmail.com>
2021-08-12 06:55:47 -0300

    Basic Seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Create Family ..."
+
+Family.create(name: "WronskiStelmach", description: "Nosso Budget Familiar")
+
+puts "End Create Family ..."
+
+puts "Init Create Categories ..."
+
+["Internet e Celular", "Mercado", "Restaurantes", "Diarista", "Pet", "Condominio", "Luz", "Saude", "Fraldas", "Farmacia", "Bebidas", "Compras", "Academia", "Outros"].each do |cat|
+    Category.create(name: cat)
+end
+
+puts "End Create Categories"
